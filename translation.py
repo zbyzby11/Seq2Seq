ce4b8cb14c4091aa5c75704fa42424da6b9c9b08
zbyzby11 <2533449732@qq.com>
2019-09-03 17:20:04 +0800

    model

--- translation.py
+++ translation.py
@@ -1,3 +1,11 @@
 """
 进行翻译的主文件
 """
+import torch
+from torch import nn, optim
+from torch.nn import functional as F
+from model import Encoder, Decoder, Seq2Seq
+
+class Translation(object):
+    def __init__(self):
+        ...
